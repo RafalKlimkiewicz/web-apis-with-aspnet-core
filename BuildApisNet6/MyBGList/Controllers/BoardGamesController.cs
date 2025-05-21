0a90410043a5cbb811533b606f36217ac7b719e8
Rafal K. <klimek1008@gmail.com>
2025-05-22 00:11:41 +0200

    ch5|CRUD

--- BuildApisNet6/MyBGList/Controllers/BoardGamesController.cs
+++ BuildApisNet6/MyBGList/Controllers/BoardGamesController.cs
@@ -11,10 +11,12 @@ namespace MyBGList.Controllers
     public class BoardGamesController : ControllerBase
     {
         private readonly ILogger<BoardGamesController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public BoardGamesController(ILogger<BoardGamesController> logger)
+        public BoardGamesController(ILogger<BoardGamesController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [HttpGet(Name = "GetBoardGames")]
@@ -22,31 +24,11 @@ namespace MyBGList.Controllers
         [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
         public RestDTO<BoardGame[]> Get()
         {
+            var query = _context.BoardGames;
+
             return new RestDTO<BoardGame[]>()
             {
-                Data = new BoardGame[] {
-                    new BoardGame() {
-                        Id = 1,
-                        Name = "Axis & Allies",
-                        Year = 1981,
-                        MinPlayers = 2,
-                        MaxPlayers = 5
-                    },
-                    new BoardGame() {
-                        Id = 2,
-                        Name = "Citadels",
-                        Year = 2000,
-                        MinPlayers = 2,
-                        MaxPlayers = 8
-                    },
-                    new BoardGame() {
-                        Id = 3,
-                        Name = "Terraforming Mars",
-                        Year = 2016,
-                        MinPlayers = 1,
-                        MaxPlayers = 5
-                    }
-                },
+                Data = query.ToArray(),
                 Links = new List<LinkDTO> { new(Url.Action(null, "BoardGames", null, Request.Scheme)!, "self", "GET") }
             };
         }
